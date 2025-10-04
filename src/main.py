a7cd48d54c28fed964e6f31332d514fc25d91c6f
Meta-Dusk <andredcxxv@gmail.com>
2025-10-04 20:25:59 +0800

    Attempting to update to flet 0.70.0.dev6214

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from main_ui import main_app
 from setup import before_main_app
 
 # TODO: Implement a feature to disable/enable debug mode
-DEBUG = True
+DEBUG = False
 
 
 async def main(page: ft.Page):
